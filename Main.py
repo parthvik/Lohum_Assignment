bf80cc377f9d46d9f06af7d7abaa8c386840640f
parthvik <66945298+parthvik@users.noreply.github.com>
2023-05-22 16:50:28 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from bs4 import BeautifulSoup
+import requests
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+        url = "https://www.metal.com/Lithium-ion-Battery/202303240001"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        avg=soup.find('span','strong___1JlBD priceDown___2TbRQ')
+        return {"Average:":avg.text}
+@app.get("/{link_id}")
+async def read_link(link_id: str):
+    # Check if the link is valid (e.g., starts with "https://")
+    if not link_id.startswith("https://"):
+        raise HTTPException(status_code=400, detail="Invalid link provided.")
+
+    # Your logic for handling valid links goes here
+    return {"link_id": link_id}
